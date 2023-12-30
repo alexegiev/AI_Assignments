89a5ac4014d8d6e35e078824fcb7cf5b387b749d
adrianakrg <adrikara2002@gmail.com>
2023-12-30 16:31:55 +0200

    added test for adaboost but its not working

--- Project2/adaboost.py
+++ Project2/adaboost.py
@@ -1,32 +1,72 @@
 import numpy as np
-class adaboost:
-    def __init__(self, n_estimators=50, learning_rate=1.0):
+class AdaBoost:
+    def __init__(self, n_estimators=5, learning_rate=1.0):
         self.n_estimators = n_estimators
         self.learning_rate = learning_rate
         self.models = []
         self.alphas = []
-        self.classes = None
 
     def fit(self, X, y):
-        self.classes = np.unique(y)
-        y = np.where(y == self.classes[0], 1, -1)
-        weights = np.ones(len(X)) / len(X)
+        n_samples, n_features = X.shape
+        weights = np.ones(n_samples) / n_samples
+
         for _ in range(self.n_estimators):
-            stump = DecisionTreeClassifier(max_depth=1)
-            stump.fit(X, y, sample_weight=weights)
-            predictions = stump.predict(X)
-            error = weights[(predictions != y)].sum()
-            alpha = self.learning_rate * (np.log(1 - error) - np.log(error)) + np.log(len(self.classes) - 1)
-            weights *= np.exp(alpha * y * predictions)
-            weights /= weights.sum()
+            stump = Stump()
+            min_error = float('inf')
+
+            for feature_i in range(n_features):
+                feature_values = np.expand_dims(X[:, feature_i], axis=1)
+                unique_values = np.unique(feature_values)
+
+                for threshold in unique_values:
+                    p = 1
+                    prediction = np.ones(np.shape(y))
+
+                    prediction[X[:, feature_i] < threshold] = -1
+
+                    error = sum(weights[y != prediction])
+
+                    if error > 0.5:
+                        error = 1 - error
+                        p = -1
+
+                    if error < min_error:
+                        stump.polarity = p
+                        stump.threshold = threshold
+                        stump.feature_index = feature_i
+                        min_error = error
+
+            stump.alpha = self.learning_rate * (np.log((1.0 - min_error) / (min_error + 1e-10)))
+            weights *= np.exp(-stump.alpha * y * stump.predict(X))
+            weights /= np.sum(weights)
+
             self.models.append(stump)
-            self.alphas.append(alpha)
 
     def predict(self, X):
-        predictions = []
-        for x in X:
-            class_scores = np.zeros(len(self.classes))
-            for i, model in enumerate(self.models):
-                class_scores[self.classes == model.predict(x)[0]] += self.alphas[i]
-            predictions.append(self.classes[np.argmax(class_scores)])
-        return predictions      
+        n_samples = X.shape[0]
+        y_pred = np.zeros((n_samples, 1))
+
+        for stump in self.models:
+            predictions = stump.predict(X)
+            y_pred += stump.alpha * predictions
+
+        y_pred = np.sign(y_pred).flatten()
+
+        return y_pred
+class Stump:
+    def __init__(self):
+        self.polarity = 1
+        self.feature_index = None
+        self.threshold = None
+        self.alpha = None
+
+    def predict(self, X):
+        n_samples = X.shape[0]
+        X_column = X[:, self.feature_index]
+        predictions = np.ones(n_samples)
+        if self.polarity == 1:
+            predictions[X_column < self.threshold] = -1
+        else:
+            predictions[X_column > self.threshold] = -1
+
+        return predictions
---
+++ Project2/test.py
@@ -0,0 +1,74 @@
+from keras.datasets import imdb
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from bayes import NaiveBayes  # assuming bayes.py is in the same directory
+from collections import Counter
+
+# Load the IMDB dataset
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+# Convert the list of words to sentences
+word_index = imdb.get_word_index()
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+train_sentences = [" ".join([reverse_word_index.get(i - 3, "?") for i in sentence]) for sentence in train_data]
+test_sentences = [" ".join([reverse_word_index.get(i - 3, "?") for i in sentence]) for sentence in test_data]
+
+# Define the hyperparameters
+n = 10
+m = 1000
+k = 10
+
+# Convert the reviews into a list of words
+train_words = [word for sentence in train_sentences for word in sentence.split()]
+
+# Count the frequency of each word
+word_freq = Counter(train_words)
+
+# Exclude the n most frequent and the k rarest words
+vocab = {word for word, freq in word_freq.most_common()[n:-k]}
+
+# Only keep the m most frequent words
+vocab = list(vocab)[:m]
+
+# Create a dictionary that maps each word to its index in the vocabulary
+word_index = {word: i for i, word in enumerate(vocab)}
+
+# Convert the reviews into binary feature vectors
+train_vectors = [[1 if word in sentence.split() else 0 for word in vocab] for sentence in train_sentences]
+test_vectors = [[1 if word in sentence.split() else 0 for word in vocab] for sentence in test_sentences]
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Train and evaluate the Naive Bayes classifier
+nb = MultinomialNB()
+nb.fit(train_vectors, train_labels)
+nb_predictions = nb.predict(test_vectors)
+print("Naive Bayes Accuracy: ", accuracy_score(test_labels, nb_predictions))
+print("Naive Bayes Precision: ", precision_score(test_labels, nb_predictions))
+print("Naive Bayes Recall: ", recall_score(test_labels, nb_predictions))
+print("Naive Bayes F1 Score: ", f1_score(test_labels, nb_predictions))
+
+# Plot learning curves
+train_sizes, train_scores, test_scores = learning_curve(MultinomialNB(), train_vectors, train_labels, cv=5)
+
+train_scores_mean = np.mean(train_scores, axis=1)
+test_scores_mean = np.mean(test_scores, axis=1)
+
+plt.plot(train_sizes, train_scores_mean, label='Training score')
+plt.plot(train_sizes, test_scores_mean, label='Cross-validation score')
+plt.title('Learning Curves')
+plt.xlabel('Training examples')
+plt.ylabel('Score')
+plt.legend(loc='best')
+plt.show()
+
+# Predict labels for the test data
+nb_predictions = nb.predict(test_vectors)
+
+# Print out some of the reviews with their predicted labels
+for review, label in zip(test_sentences[:10], nb_predictions[:10]):
+    print(f"Review: {review}\nPredicted label: {'Good' if label == 1 else 'Bad'}\n")
